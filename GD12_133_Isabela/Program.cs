64fccd1c7b753f036a84a971f4247fbe60380116
isapinello <isapinelloamorim@gmail.com>
2024-09-06 12:10:48 -0700

    Lab 1 file upload
    First project

---
+++ GD12_133_Isabela/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_133_Isabela
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
