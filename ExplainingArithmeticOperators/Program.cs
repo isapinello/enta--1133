bb30cf637291c11ad43026e115d8ee1919371a46
isapinello <isapinelloamorim@gmail.com>
2024-09-20 00:34:22 -0700

    ExplanationOfArithmeticOperators
    Basic information about operators

---
+++ ExplainingArithmeticOperators/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExplainingArithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using +");
+            Console.WriteLine("Adds the two values between the operator");
+
+            Console.WriteLine("Using -");
+            Console.WriteLine("Subtracts one value from another");
+
+            Console.WriteLine("Using /");
+            Console.WriteLine("Divides one value by another");
+
+            Console.WriteLine("Using *");
+            Console.WriteLine("Multiplies the values");
+
+            Console.WriteLine("Using ++");
+            Console.WriteLine("Increases the value of a variable by 1");
+
+            Console.WriteLine("Using --");
+            Console.WriteLine("Decreases the value of a variable by 1");
+
+            Console.WriteLine("Using %");
+            Console.WriteLine("Shows the remaining number from the solution of a division between two values");
+            Console.WriteLine("For example:");
+            Console.WriteLine("If we have 5 % 2"); //Not in coding language, just a metaphor for better understanding
+            Console.WriteLine("The number shown will be 1");
+        }
+    }
+}
